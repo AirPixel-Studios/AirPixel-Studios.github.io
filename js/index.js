a92a7daa486cc83d7ecc22094ab39a380f6ee2d4
Anh Duc Tran <anh.duc.tran@sap.com>
2023-05-28 21:48:47 +0200

    Add translation stuff

---
+++ js/index.js
@@ -0,0 +1,42 @@
+//Function called on page load
+window.onload = function () {
+  setInitialLangCookie();
+};
+
+//Set first cookie when the user is on the page for the first time
+function setInitialLangCookie() {
+  let cookie = Cookies.get("lang");
+
+  if (cookie === undefined) {
+    Cookies.set("lang", "EN");
+    switchLang(Cookies.get("lang"));
+  } else {
+    switchLang(Cookies.get("lang"));
+  }
+}
+
+//Language switch event
+$("#switch-lang").click(function (event) {
+  event.preventDefault();
+  switchLang(event.target.innerText);
+});
+
+//Change the language of the selected elements on the page
+//Note: Each language has its own if case. If we add the Korean language in the future, we can easily extend this function.
+function switchLang(lang) {
+  if (lang === "DE") {
+    Cookies.set("lang", "DE");
+    $('[lang="en"]').hide();
+    $('[lang="de"]').show();
+
+    $("#lang-de-btn").addClass("focus");
+    $("#lang-en-btn").removeClass("focus");
+  } else if (lang === "EN") {
+    Cookies.set("lang", "EN");
+    $('[lang="de"]').hide();
+    $('[lang="en"]').show();
+
+    $("#lang-en-btn").addClass("focus");
+    $("#lang-de-btn").removeClass("focus");
+  }
+}
