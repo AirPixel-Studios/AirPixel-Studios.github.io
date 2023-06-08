eadbb7ec009990831b8f802e84916e621f3bb5b3
Anh Duc Tran <anh.duc.tran@sap.com>
2023-06-08 20:58:40 +0200

    code changes

--- js/index.js
+++ js/index.js
@@ -2,10 +2,27 @@
 $(document).ready(function () {
   "use strict";
 
-  $.getScript("../assets/vendor/jquery-validation/dist/localization/messages_" + Cookies.get("lang") + ".js");
+  $(".portfolio-slick").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true, 
+  });
+
+  $.getScript(
+    "../assets/vendor/jquery-validation/dist/localization/messages_" +
+      Cookies.get("lang") +
+      ".js"
+  );
 
   //Language switch event
   $("#switch-lang").click(function (event) {
-    $.getScript("../assets/vendor/jquery-validation/dist/localization/messages_" + Cookies.get("lang") + ".js");
+    $.getScript(
+      "../assets/vendor/jquery-validation/dist/localization/messages_" +
+        Cookies.get("lang") +
+        ".js"
+    );
   });
 });
