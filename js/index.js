5b208d8826fc239ad08f8317280580c07174c863
Anh Duc Tran <anh.duc.tran@sap.com>
2023-06-09 03:52:51 +0200

    delete videojs

--- js/index.js
+++ js/index.js
@@ -2,19 +2,6 @@
 $(document).ready(function () {
   "use strict";
 
-  videojs('page-background-video', {
-    autoplay: true,
-    controls: false,
-    loop: true,
-    muted: true,
-    fluid: true,
-    preload: 'auto',
-    poster: 'assets/videos/Background.mp4',
-    sources: [
-        { src: 'assets/videos/Background.mp4', type: 'video/mp4' }
-    ]
-});
-
   $.getScript("../assets/vendor/jquery-validation/dist/localization/messages_" + Cookies.get("lang") + ".js");
 
   //Language switch event
